c6df56ab68dfb6cdca5c83448392293ff70ce0b1
xiangbin0712 <mine7eleven@gmail.com>
2020-01-17 12:34:43 +0800

    搜索主页组件封装

---
+++ unpackage/dist/dev/app-plus/pages/playMenuSquare/playMenuSquare.js
@@ -0,0 +1,2 @@
+setCssToHead([".",[1],"container.",[1],"data-v-c9a2489c { margin: 0 ",[0,30],"; }\n",],undefined,{path:"./pages/playMenuSquare/playMenuSquare.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/playMenuSquare/playMenuSquare.wxml') } }));
